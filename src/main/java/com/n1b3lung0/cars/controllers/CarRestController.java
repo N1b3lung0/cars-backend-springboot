7e383b539a662cfb321d0f781770a01529b21b5f
N1b3lung0 <carlos.martinez.crespo@gmail.com>
2020-05-24 01:05:00 +0200

    feat: added CrossOrigin to connect with our Angular front app

--- src/main/java/com/n1b3lung0/cars/controllers/CarRestController.java
+++ src/main/java/com/n1b3lung0/cars/controllers/CarRestController.java
@@ -3,14 +3,16 @@ package com.n1b3lung0.cars.controllers;
 import com.n1b3lung0.cars.models.entity.Car;
 import com.n1b3lung0.cars.models.services.ICarService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@CrossOrigin(origins = { "http://localhost:4200" })
 @RestController
-@RequestMapping("/car")
+@RequestMapping("/cars")
 public class CarRestController {
 
     @Autowired
