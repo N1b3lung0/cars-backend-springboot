eaa91ce73a414905dfebb8c23f5910b7ee904c92
N1b3lung0 <carlos.martinez.crespo@gmail.com>
2020-05-24 00:16:35 +0200

    feat: Implemented findAll() method

--- src/main/java/com/n1b3lung0/cars/models/services/ICarServiceImpl.java
+++ src/main/java/com/n1b3lung0/cars/models/services/ICarServiceImpl.java
@@ -1,4 +1,22 @@
 package com.n1b3lung0.cars.models.services;
 
-public class ICarServiceImpl {
+import com.n1b3lung0.cars.models.dao.ICarDao;
+import com.n1b3lung0.cars.models.entity.Car;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+public class ICarServiceImpl implements ICarService {
+
+    @Autowired
+    private ICarDao carDao;
+
+    @Override
+    @Transactional(readOnly = true)
+    public List<Car> findAll() {
+        return (List<Car>) carDao.findAll();
+    }
 }
