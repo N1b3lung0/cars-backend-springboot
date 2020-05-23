804fd035f4da120864bf3f1909a6dadc74120324
N1b3lung0 <carlos.martinez.crespo@gmail.com>
2020-05-24 00:12:30 +0200

    feat: Entity Car modified adding attributes

--- src/main/java/com/n1b3lung0/cars/models/entity/Car.java
+++ src/main/java/com/n1b3lung0/cars/models/entity/Car.java
@@ -1,4 +1,27 @@
 package com.n1b3lung0.cars.models.entity;
 
-public class Car {
+import lombok.Data;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Date;
+
+@Entity
+@Table(name = "cars")
+@Data
+public class Car implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String brand;
+
+    @Column(name = "create_at")
+    @Temporal(TemporalType.DATE)
+    private Date createAt;
+
+    private static final long serialVersionUID = 1L;
 }
