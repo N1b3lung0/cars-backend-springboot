183a98bc957983aed2bdfeb0ea1ad93b08c79af9
N1b3lung0 <carlos.martinez.crespo@gmail.com>
2020-05-24 00:27:02 +0200

    feat: modified CarRestController adding a new endpoint to get all Cars

--- src/main/java/com/n1b3lung0/cars/controllers/CarRestController.java
+++ src/main/java/com/n1b3lung0/cars/controllers/CarRestController.java
@@ -1,4 +1,23 @@
 package com.n1b3lung0.cars.controllers;
 
+import com.n1b3lung0.cars.models.entity.Car;
+import com.n1b3lung0.cars.models.services.ICarService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/car")
 public class CarRestController {
+
+    @Autowired
+    private ICarService carService;
+
+    @GetMapping(value = { "", "/" })
+    public List<Car> index() {
+        return carService.findAll();
+    }
 }
