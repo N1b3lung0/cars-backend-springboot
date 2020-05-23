2e719e532d35544cc60f698acf2d140bbfea8fdc
N1b3lung0 <carlos.martinez.crespo@gmail.com>
2020-05-24 00:15:22 +0200

    feat: Interface ICarService modified adding findAll() method

--- src/main/java/com/n1b3lung0/cars/models/services/ICarService.java
+++ src/main/java/com/n1b3lung0/cars/models/services/ICarService.java
@@ -1,4 +1,10 @@
 package com.n1b3lung0.cars.models.services;
 
+import com.n1b3lung0.cars.models.entity.Car;
+
+import java.util.List;
+
 public interface ICarService {
+
+    public List<Car> findAll();
 }
