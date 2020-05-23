48ffdd9b5a82b22905467ee79ba9e2b64a5cd2f6
N1b3lung0 <carlos.martinez.crespo@gmail.com>
2020-05-24 00:36:11 +0200

    feat: added a few inserts to test the endpoint

---
+++ src/main/resources/import.sql
@@ -0,0 +1,6 @@
+/* Populate Car table */
+
+INSERT INTO cars (name, brand, create_at) VALUES ('A5', 'Audi', '2020-02-01');
+INSERT INTO cars (name, brand, create_at) VALUES ('Serie 4', 'BMW', '2020-03-21');
+INSERT INTO cars (name, brand, create_at) VALUES ('Clase C Coupé', 'Mercedes', '2020-01-17');
+INSERT INTO cars (name, brand, create_at) VALUES ('Leon', 'SEAT', '2020-04-12');
