da01ca04b85af231c625bf53f06e95154491bc67
N1b3lung0 <carlos.martinez.crespo@gmail.com>
2020-05-26 19:06:19 +0200

    feat: included prePersist to add the date in the creation of a new car

--- src/main/java/com/n1b3lung0/cars/models/entity/Car.java
+++ src/main/java/com/n1b3lung0/cars/models/entity/Car.java
@@ -23,5 +23,10 @@ public class Car implements Serializable {
     @Temporal(TemporalType.DATE)
     private Date createAt;
 
+    @PrePersist
+    public void prePersist() {
+        createAt = new Date();
+    }
+
     private static final long serialVersionUID = 1L;
 }
