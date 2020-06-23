c2dd35f0494868243e2891766333a4a226f6ed68
Carlos Martínez Crespo <carlos.martinez.crespo@gmail.com>
2020-06-23 18:51:16 +0200

    feat: added a new property (photo)

--- src/main/java/com/n1b3lung0/cars/models/entity/Car.java
+++ src/main/java/com/n1b3lung0/cars/models/entity/Car.java
@@ -2,6 +2,7 @@ package com.n1b3lung0.cars.models.entity;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import java.io.Serializable;
 import java.util.Date;
@@ -23,20 +24,26 @@ public class Car implements Serializable {
     @Column(nullable = false) // unique = true
     private String brand;
 
+    @NotNull
     @Column(name = "create_at")
     @Temporal(TemporalType.DATE)
     private Date createAt;
 
-    @PrePersist
-    public void     prePersist()            { createAt = new Date(); }
+    private String photo;
 
-    public Long     getId()                 { return id; }
-    public void     setId(Long id)          { this.id = id; }
-    public String   getName()               { return name; }
-    public void     setName(String name)    { this.name = name; }
-    public String   getBrand()              { return brand; }
-    public void     setBrand(String brand)  { this.brand = brand; }
-    public Date     getCreateAt()           { return createAt; }
+    //@PrePersist
+    //public void     prePersist()            { createAt = new Date(); }
+
+    public Long     getId()                     { return id; }
+    public void     setId(Long id)              { this.id = id; }
+    public String   getName()                   { return name; }
+    public void     setName(String name)        { this.name = name; }
+    public String   getBrand()                  { return brand; }
+    public void     setBrand(String brand)      { this.brand = brand; }
+    public Date     getCreateAt()               { return createAt; }
+    public void     setCreateAt(Date createAt)  { this.createAt = createAt; }
+    public String   getPhoto()                  { return photo; }
+    public void     setPhoto(String photo)      { this.photo = photo; }
 
     public Car UpdateCar(Car carToUpdate, Car car) {
         carToUpdate.setName(car.getName());
