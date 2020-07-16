e60da7db7c663bd24674b7456e460af78c8ebb5c
N1b3lung0 <carlos.martinez.crespo@gmail.com>
2020-07-16 18:01:31 +0200

    chore: changes to upload the project to the cloud

--- src/main/java/com/n1b3lung0/cars/auth/ResourceServerConfig.java
+++ src/main/java/com/n1b3lung0/cars/auth/ResourceServerConfig.java
@@ -34,7 +34,7 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:4200"));
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:4200", "*"));
         configuration.setAllowedMethods(Arrays.asList("GET","POST","PUT","DELETE","OPTIONS"));
         configuration.setAllowCredentials(true);
         configuration.setAllowedHeaders(Arrays.asList("Content-Type","Authorization"));
